ad121e7b79300b0b371c9f95c939ea8155072ee7
Joan Fernandez <hincapiefernandezjoan123@gmail.com>
2024-07-30 08:08:08 -0500

    finalización del formulario

---
+++ app.js
@@ -0,0 +1,21 @@
+const formEmail = document.getElementById("frm-email")
+formEmail.addEventListener("submit", sendEmail)
+
+const serviceId = "service_dq66346"
+const templateId ="template_1mz25dn"
+const apiKey = "tWYqMt6aWEVp4Qom1"
+
+function sendEmail(event){
+    event.preventDefault()
+    emailjs.init(serviceId)
+
+    emailjs.sendForm(serviceId,templateId,formEmail,apiKey)
+    .then(result => Swal.fire("Su mensaje se ha enviado correctamente") )
+    .catch(error=>{
+        Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: "Algo ha salido mal!, No se ha podido enviar tu mensaje",
+        });
+    })
+}
